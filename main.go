a186afb0bfbaf92c509a4229b070fb3bd579134d
xfp-discerning <xfp.discerning@gmail.com>
2022-10-14 21:16:43 +0800

    20221014

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("1111")
+	//1.创建路由
 	r := gin.Default()
+	//2.绑定路由规则
+	//gin.Context封装了request和response
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "xfp")
+	})
+	//监听端口
+	r.Run("localhost:8000")
 }
